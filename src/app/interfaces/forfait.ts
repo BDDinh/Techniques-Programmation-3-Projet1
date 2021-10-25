5a37f7a8e19c3a1a3f8894ddbcd8b8003194dcef
BD Dinh <bd.dinh@gmail.com>
2021-10-24 20:56:09 -0400

    1ere commit

---
+++ src/app/interfaces/forfait.ts
@@ -0,0 +1,12 @@
+import { Hotel } from "./hotel";
+
+export interface Forfait {
+    destination: string;
+    villeDepart: string;
+    hotel: Hotel;
+    dateDepart: Date;
+    dateRetour: Date;
+    prix:number;
+}
+
+
